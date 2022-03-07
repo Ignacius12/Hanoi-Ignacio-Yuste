8bd449082e98d6b05d36bad56f063d913ec186d0
Ignacio <iyusazn@epsg.upv.es>
2022-03-07 16:10:11 +0100

    Cambios en mover disco

--- Torres_de_Hanoi/Pila.cs
+++ Torres_de_Hanoi/Pila.cs
@@ -9,34 +9,47 @@ namespace Torres_de_Hanoi
     class Pila
     {
         public int Size { get; set; }
-        /* TODO: Elegir tipo de Top
+
+        //TODO: Elegir tipo de Top
         public int Top { get; set; }
-        public String Top { get; set; }        
-        */
-        /* TODO: Elegir tipo de Elementos
-        public Disco[] Elementos { get; set; }
+                
+        
+        // TODO: Elegir tipo de Elementos
+       
         public List<Disco> Elementos { get; set; }
-        */
+        
 
         /* TODO: Implementar métodos */
         public Pila()
         {
-
+            Size = 0;
+            Top = 0;
+            Elementos = new List<Disco>();
         }
 
         public void push(Disco d)
         {
-
+            Elementos.Add(d);
+            Size++;
+            Top = d.Valor;
         }
 
         public Disco pop()
         {
-            return null;
+            Disco quitado= Elementos.ElementAt(Top);
+            Elementos.RemoveAt(Top);
+            Size--;
+            return quitado;
         }                
 
         public bool isEmpty()
         {
-            return true;
+            if (Size == 0)
+            {
+                return true;
+            }
+            return false;
+            
         }
 
     }
